b604a59e9b663e64de6e2f17aa710e082bca532c
Jose <jozylopez27@gmail.com>
2020-03-20 12:41:07 -0500

    fixed db error

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/brewme',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/brewme',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/brewme-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
