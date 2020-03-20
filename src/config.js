ef58bc850f48ff7bb8511f3b2224e304cb4dd097
Jose <jozylopez27@gmail.com>
2020-03-20 11:48:52 -0500

    changed database settings

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/brewme',
+    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/brewme',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/brewme-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
